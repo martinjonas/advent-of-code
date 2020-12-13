fe6ed68a9da55f2e0f444863d594f377d690e925
Martin Jonáš <mjonas@fbk.eu>
2020-12-13 14:15:18 +0100

    Add nicer solution to Day 13.

--- 2020-go/Day13/main.go
+++ 2020-go/Day13/main.go
@@ -26,36 +26,51 @@ func part1(target int, busIds []int) int {
 	return min * minId
 }
 
-func part2(target int, busIds []int) int {
-	current := 0
-	step := 1
+func part2(busIds []int) int {
+	current, step := 0, 1
 
-	for i, busId := range busIds {
-		if busId != 0 {
-			currentTarget := busId - i
+	for i, modulo := range busIds {
+		if modulo != 0 {
+			currentTarget := modulo - i
 
-			// solve "current + x*step == currentTarget (mod busId)" for x
-			// x = (currentTarget - current)*(step^{-1}) (mod busId)
+			// solve "current + x*step == currentTarget (mod modulo)" for x
+			// x = (currentTarget - current)*(step^{-1}) (mod modulo)
 
 			// difference = (currentTarget - current)
-			difference := (currentTarget - current + busId) % busId
+			difference := (currentTarget - current + modulo) % modulo
 
-			// compute (step^{-1}) (mod busId)
+			// compute (step^{-1}) (mod modulo)
 			var necessarySteps big.Int
-			necessarySteps.ModInverse(big.NewInt(int64(step)), big.NewInt(int64(busId)))
+			necessarySteps.ModInverse(big.NewInt(int64(step)), big.NewInt(int64(modulo)))
 
 			// add x*step, which is (currentTarget - current)*(step^{-1})*step
-			// the inversion of step does not cancel out, as it is computed modulo busId
+			// the inversion of step does not cancel out, as it is computed modulo modulo
 			// and we are working modulo lcm of bus intervals
 			current += difference * int(necessarySteps.Int64()) * step
 
-			step *= busId
+			step *= modulo
 		}
 	}
 
 	return current % step
 }
 
+func part2_sieve(busIds []int) int {
+	// I swear that this did not work when I tried the sieving method
+	// in the morning…
+	current, step := 0, 1
+
+	for target, modulo := range busIds {
+		if modulo != 0 {
+			for (- target - current) % modulo != 0 {
+				current += step
+			}
+			step *= modulo
+		}
+	}
+
+	return current
+}
 
 func main() {
 	file, _ := os.Open("input")
@@ -80,5 +95,6 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(target, busIds))
-	fmt.Printf("Part 2: %d\n", part2(target, busIds))
+	fmt.Printf("Part 2: %d\n", part2(busIds))
+	fmt.Printf("Part 2 (sieve): %d\n", part2_sieve(busIds))
 }
