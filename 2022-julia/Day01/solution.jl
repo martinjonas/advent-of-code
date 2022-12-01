20c0080cab6b8a99974cd7a6f2df740c68f04df1
Martin Jonas <mjonas@fbk.eu>
2022-12-01 07:22:48 +0100

    A bit nicer solution to Day 01.

--- 2022-julia/Day01/solution.jl
+++ 2022-julia/Day01/solution.jl
@@ -1,17 +1,13 @@
-function solve(lines::Vector{String})
-    sums = [0]
-    for line in lines
-        if line == ""
-            push!(sums, 0)
-        else
-            sums[end] += parse(Int, line)
-        end
-    end
+int(x) = parse(Int, x)
+
+function solve(input::String)
+    groups = split(input, "\n\n") .|> split
+    sums = sum.(int, groups)
 
     println(maximum(sums))
-    println(sum(sort(sums, rev=true)[1:3]))
+    println(sum(partialsort(sums, 1:3, rev=true)))
 end
 
 filename = (length(ARGS) == 0) ? "input" : ARGS[1]
-lines = readlines(filename)
-@time solve(lines)
+input = read(filename, String)
+@time solve(input)
