0497a87123813003ea1d3c9de06ff365ee9aceea
Martin Jonas <mjonas@fbk.eu>
2022-12-05 08:07:41 +0100

    Cleanup day 5.

--- 2022-julia/Day05/solution.jl
+++ 2022-julia/Day05/solution.jl
@@ -9,6 +9,19 @@ struct Move
     to::Int
 end
 
+function apply_move!(move::Move, state)
+    for _ in 1:move.num
+        push!(state[move.to], pop!(state[move.from]))
+    end
+end
+
+function apply_revmove!(move::Move, state)
+    append!(state[move.to], last(state[move.from], move.num))
+    for _ ∈ 1:move.num
+        pop!(state[move.from])
+    end
+end
+
 function parse_state(state)
     lines = split(state, "\n")
 
@@ -33,35 +46,12 @@ function parse_instructions(instructions)
     [Move(map(int, match(r, line).captures)...) for line in lines]
 end
 
-function part1(input)
+function solve(input, move_fun!)
     state = parse_state(input[1])
     instructions = parse_instructions(input[2])
-
-    for move ∈ instructions
-        for _ in 1:move.num
-            push!(state[move.to], pop!(state[move.from]))
-        end
-    end
-
+    foreach(m -> move_fun!(m, state), instructions)
     join(map(last, state))
 end
 
-function part2(input)
-    state = parse_state(input[1])
-    instructions = parse_instructions(input[2])
-
-    for move ∈ instructions
-        to_move = last(state[move.from], move.num)
-
-        for _ ∈ 1:move.num
-            pop!(state[move.from])
-        end
-
-        append!(state[move.to], to_move)
-    end
-
-    join(map(last, state))
-end
-
-input |> part1 |> println
-input |> part2 |> println
+solve(input, apply_move!) |> println
+solve(input, apply_revmove!) |> println
