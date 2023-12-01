5b2e35876aee291dd62fff6bdbd0d94bcb73ae56
Martin Jonas <mjonas@fbk.eu>
2023-12-01 07:22:04 +0100

    A bit nicer part 2.

--- 2023-rust/day01/src/main.rs
+++ 2023-rust/day01/src/main.rs
@@ -22,21 +22,17 @@ fn part1(input: &[String]) -> u32 {
     res
 }
 
+static NUMERALS : [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
 fn part2(input: &[String]) -> u32 {
     let mut res: u32 = 0;
     for line in input {
         let mut digits = Vec::<u32>::new();
         for i in 0..line.len() {
             let prefix = &line[i..line.len()];
-            if prefix.starts_with("one") { digits.push(1) };
-            if prefix.starts_with("two") { digits.push(2) };
-            if prefix.starts_with("three") { digits.push(3) };
-            if prefix.starts_with("four") { digits.push(4) };
-            if prefix.starts_with("five") { digits.push(5) };
-            if prefix.starts_with("six") { digits.push(6) };
-            if prefix.starts_with("seven") { digits.push(7) };
-            if prefix.starts_with("eight") { digits.push(8) };
-            if prefix.starts_with("nine") { digits.push(9) };
+            for (v, n) in (1..).zip(NUMERALS) {
+                if prefix.starts_with(n) { digits.push(v) };
+            }
 
             let first = prefix.chars().next().unwrap();
             if let Some(d) = first.to_digit(10) {
